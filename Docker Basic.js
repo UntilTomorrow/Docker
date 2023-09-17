a435066fb596693b030c826e2238b2811e89934e
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-09-17 21:03:59 +0700

    Update Docker Basic.js

--- Docker Basic.js
+++ Docker Basic.js
@@ -1,3 +1,9 @@
 Basic 
 
-Docker run -it (os/service) sh
+docker run -it (os/service) sh
+docker run -it -e NAME=ubuntku ubuntu sh
+docker ps 
+docker ps a  
+docker container stop id
+docker container start id
+docker rm id
