55a217de9e3d62d22e1c4212da09728e26a62b73
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-11-10 16:29:26 +0700

    Update DockerBasic.js

--- DockerBasic.js
+++ DockerBasic.js
@@ -12,6 +12,7 @@ docker run -p 80:80 imagename
 docker run exec -it idcontainer bash
 
 docker commit my_container my_custom_image
+docker tag nama-gambar:tag-lama nama-gambar:tag-baru
 docker tag my_custom_image:latest username/my_custom_image:latest
 docker push username/my_custom_image:latest
 docker run -d -p 80:80 --name my_new_container username/my_custom_image:latest
